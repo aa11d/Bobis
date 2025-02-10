694302eff751a5f5d0411cd5aa6189b6beabe572
kecske1391 <bobisbarnabas@gmail.com>
2025-02-10 14:57:22 +0100

    Lista cucc

---
+++ Hoeses/osszead/lista.cs
@@ -0,0 +1,17 @@
+class Lista
+{
+    const int length = 80;
+    static Random rnd = new();
+    static List<int> fromleft = new List<int>();
+    static List<int> fromright = [];
+    static void NewNumber (double prob){
+        if (rnd.NextDouble() < prob)
+        {
+            
+        }
+    }
+    static void Main(){
+        Console.SetWindowSize(85,10);
+        
+    }
+}
