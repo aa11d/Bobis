fc10670420cf0d2177d5d785f6ed4dbb3685b323
kecske1391 <116422356+kecske1391@users.noreply.github.com>
2025-03-17 11:23:28 +0100

    Add files via upload
    Lapok

---
+++ Lapok/Lapok/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Lapok
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.Location = new Point(43, 34);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 75);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += Lap_katt;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Green;
+            button2.Location = new Point(124, 34);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 75);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += Lap_katt;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Green;
+            button3.Location = new Point(205, 34);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 75);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += Lap_katt;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Green;
+            button4.Location = new Point(286, 34);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 75);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += Lap_katt;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Green;
+            button5.Location = new Point(43, 115);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 75);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += Lap_katt;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Green;
+            button6.Location = new Point(124, 115);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 75);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += Lap_katt;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Green;
+            button7.Location = new Point(205, 115);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 75);
+            button7.TabIndex = 6;
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += Lap_katt;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Green;
+            button8.Location = new Point(286, 115);
+            button8.Name = "button8";
+            button8.Size = new Size(75, 75);
+            button8.TabIndex = 7;
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += Lap_katt;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.Green;
+            button9.Location = new Point(43, 196);
+            button9.Name = "button9";
+            button9.Size = new Size(75, 75);
+            button9.TabIndex = 8;
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += Lap_katt;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.Green;
+            button10.Location = new Point(124, 196);
+            button10.Name = "button10";
+            button10.Size = new Size(75, 75);
+            button10.TabIndex = 9;
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += Lap_katt;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.Green;
+            button11.Location = new Point(205, 196);
+            button11.Name = "button11";
+            button11.Size = new Size(75, 75);
+            button11.TabIndex = 10;
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += Lap_katt;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.Green;
+            button12.Location = new Point(286, 196);
+            button12.Name = "button12";
+            button12.Size = new Size(75, 75);
+            button12.TabIndex = 11;
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += Lap_katt;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.Green;
+            button13.Location = new Point(43, 277);
+            button13.Name = "button13";
+            button13.Size = new Size(75, 75);
+            button13.TabIndex = 12;
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += Lap_katt;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.Green;
+            button14.Location = new Point(124, 277);
+            button14.Name = "button14";
+            button14.Size = new Size(75, 75);
+            button14.TabIndex = 13;
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += Lap_katt;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.Green;
+            button15.Location = new Point(205, 277);
+            button15.Name = "button15";
+            button15.Size = new Size(75, 75);
+            button15.TabIndex = 14;
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += Lap_katt;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.Green;
+            button16.Location = new Point(286, 277);
+            button16.Name = "button16";
+            button16.Size = new Size(75, 75);
+            button16.TabIndex = 15;
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += Lap_katt;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(517, 34);
+            button17.Name = "button17";
+            button17.Size = new Size(194, 75);
+            button17.TabIndex = 16;
+            button17.Text = "Új játék";
+            button17.UseVisualStyleBackColor = false;
+            button17.Click += New_game_katt;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 455);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Lapfordito";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+    }
+}
---
+++ Lapok/Lapok/Form1.cs
@@ -0,0 +1,99 @@
+namespace Lapok
+{
+    public partial class Form1 : Form
+    {
+        Button[,] lapok;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lapok = new Button[4, 4] { { button1, button2, button3, button4 }, { button5, button6, button7, button8 }, { button9, button10, button11, button12 }, { button13, button14, button15, button16 } };
+
+        }
+
+        private void Lap_katt(object sender, EventArgs e)
+        {
+            bool gomb = false;
+            int x = 0;
+            int y = 0;
+            while (!gomb)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (lapok[i, j] == (sender as Button))
+                        {
+                            gomb = true;
+                            x = i;
+                            y = j;
+
+                        }
+                    }
+                }
+            }
+            if (gomb)
+            {
+                Szinfordit(x, y);
+            }
+        }
+        private void Szinfordit(int x, int y)
+        {
+            //felsõ
+            if (y - 1 >= 0 && lapok[x, y - 1].BackColor == Color.Green)
+            {
+                lapok[x, y - 1].BackColor = Color.Yellow;
+            }
+            else if (y - 1 >= 0 && lapok[x, y - 1].BackColor == Color.Yellow)
+            {
+                lapok[x, y - 1].BackColor = Color.Green;
+            }
+
+            //jobb
+            if (x + 1 <= 3 && lapok[x + 1, y].BackColor == Color.Green)
+            {
+                lapok[x + 1, y].BackColor = Color.Yellow;
+            }
+            else if (x + 1 <= 3 && lapok[x + 1, y].BackColor == Color.Yellow)
+            {
+                lapok[x + 1, y].BackColor = Color.Green;
+            }
+
+            //alsó
+            if (y + 1 <= 3 && lapok[x, y + 1].BackColor == Color.Green)
+            {
+                lapok[x, y + 1].BackColor = Color.Yellow;
+            }
+            else if (y + 1 <= 3 && lapok[x, y + 1].BackColor == Color.Yellow)
+            {
+                lapok[x, y + 1].BackColor = Color.Green;
+            }
+
+            //bal
+            if (x - 1 >= 0 && lapok[x - 1, y].BackColor == Color.Green)
+            {
+                lapok[x - 1, y].BackColor = Color.Yellow;
+            }
+            else if (x - 1 >= 0 && lapok[x - 1, y].BackColor == Color.Yellow)
+            {
+                lapok[x - 1, y].BackColor = Color.Green;
+            }
+        }
+
+        private void New_game_katt(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 16; i++)
+            {
+                int rx = random.Next(0, 4);
+                int ry = random.Next(0, 4);
+                Szinfordit(rx,ry);
+            }
+            
+
+        }
+    }
+}
